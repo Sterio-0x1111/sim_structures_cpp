7332aa6a1947a20976ff452cf69d2d53f1c2dc24
Sterio-0x1111 <xxsterioxx2@gmail.com>
2026-02-10 02:08:12 +0100

    OpenGL and test for Cube Sim

---
+++ build/CMakeFiles/SimCubeLight.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimCubeLight.dir/src/main.cpp.obj"
+  "CMakeFiles/SimCubeLight.dir/src/main.cpp.obj.d"
+  "SimCubeLight.exe"
+  "SimCubeLight.exe.manifest"
+  "SimCubeLight.pdb"
+  "libSimCubeLight.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimCubeLight.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- sim_cube_light/src/main.cpp
+++ sim_cube_light/src/main.cpp
@@ -1,10 +1,57 @@
+// #include <iostream>
+//
+// using std::cout;
+// using std::endl;
+//
+// int main(int argc, char* argv[]) {
+//     Cube cube(64, 64, 64);
+//
+//     return EXIT_SUCCESS;
+// }
+
+// https://glad.dav1d.de/#language=c&specification=gl&api=gl%3D4.6&api=gles1%3Dnone&api=gles2%3Dnone&api=glsc2%3Dnone&profile=core&loader=on
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
-using std::cout;
-using std::endl;
+int main() {
+    // Initialisierung
+    if (!glfwInit())
+        return -1;
 
-int main(int argc, char* argv[]) {
-    cube(64, 64);
+    // OpenGL Version festlegen --> 4.6 Core
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    return EXIT_SUCCESS;
+    // Fenster erstellen
+    GLFWwindow *window =
+        glfwCreateWindow(800, 600, "Sim Cube Light", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // GLAD laden
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Fehler beim Laden von GLAD!" << std::endl;
+        return -1;
+    }
+
+    // Main Loop
+    while (!glfwWindowShouldClose(window)) {
+        // Hintergrundfarbe --> Dunkelgrau
+        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
